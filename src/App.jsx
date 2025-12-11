cbc5c156427c45c3fd194988b7fad3ed49581e55
adityasemalti <adityasemalti535@gmail.com>
2025-12-11 19:20:35 +0530

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,66 @@
-import React from "react";
-import Header from "./components/Header";
+import React, { useState } from "react";
 import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
 import InviteForm from "./components/InviteForm";
 import AdminFinder from "./components/AdminFinder";
 
 const App = () => {
-    return (
-        <div className="flex flex-col md:flex-row min-h-screen bg-gray-100">
-            <Sidebar />
-            <div className="flex-1 flex flex-col">
-                <div className="sticky top-0 z-20 bg-white/30 backdrop-blur-lg">
-                    <Header />
-                </div>
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
+  return (
+    <div className="flex flex-col md:flex-row min-h-screen bg-gray-100">
+      
+      {/* Sidebar */}
+      <div
+        className={`
+          fixed inset-y-0 left-0 w-64 bg-white shadow-lg transform
+          ${sidebarOpen ? "translate-x-0" : "-translate-x-full"}
+          transition-transform duration-300 ease-in-out
+          z-50 md:relative md:translate-x-0
+        `}
+      >
+        <Sidebar />
+      </div>
 
-                <div className="p-6 md:p-8 flex flex-col md:flex-row items-start md:items-start justify-center md:justify-start gap-8 flex-wrap">
-                    <div className="flex-1 min-w-[300px]">
-                        <InviteForm />
-                    </div>
+      {/* Overlay for mobile when sidebar is open */}
+      {sidebarOpen && (
+        <div
+          className="fixed inset-0 z-40 bg-black/20 md:hidden"
+          onClick={() => setSidebarOpen(false)}
+        ></div>
+      )}
 
-                    <div className="flex-1 min-w-[250px]">
-                        <AdminFinder />
-                    </div>
+      {/* Main Content */}
+      <div className="flex-1 flex flex-col md:ml-0 relative">
+        
+        {/* Header */}
+        <div className="sticky top-0 z-30 bg-white/30 backdrop-blur-lg flex items-center p-4 md:p-6">
+          
+          {/* Menu Button on Left */}
+          <button
+            className="md:hidden text-gray-800 text-2xl mr-4"
+            onClick={() => setSidebarOpen(!sidebarOpen)}
+          >
+            ☰
+          </button>
 
-                </div>
-            </div>
-
+          <Header />
         </div>
-    );
+
+        {/* Content */}
+        <div className="p-6 md:p-8 flex flex-col lg:flex-row items-start gap-8 w-full">
+          <div className="w-full lg:w-1/2">
+            <InviteForm />
+          </div>
+
+          <div className="w-full lg:w-1/2">
+            <AdminFinder />
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
 };
 
 export default App;
